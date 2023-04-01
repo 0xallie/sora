1901516db946ada77778bd3d46f29fb9db00e9da
alexia <me@alexia.lol>
2023-04-01 19:08:34 +0200

    feat(rules): make rules configurable

--- sora/cogs/rules.py
+++ sora/cogs/rules.py
@@ -18,49 +18,22 @@ class Rules(Cog):
             )
             return
 
-        await interaction.channel.send(
-            embeds=[
-                discord.Embed(
-                    color=discord.Color.fuchsia(),
-                    title="Rule 1",
-                    description=(
-                        "Be nice. Swearing is allowed, but try not to insult people. "
-                        "A little banter is fine, but if someone is uncomfortable with it, please stop."
-                    ),
+        if not (rules := self.config.get("rules")):
+            await interaction.response.send_message(
+                embed=discord.Embed(
+                    color=discord.Color.red(),
+                    description="No rules are configured for this server.",
                 ),
-                discord.Embed(
-                    color=discord.Color.fuchsia(),
-                    title="Rule 2",
-                    description="No racism, sexism, homophobia, transphobia, ableism or other bigotry.",
-                ),
-                discord.Embed(
-                    color=discord.Color.fuchsia(),
-                    title="Rule 3",
-                    description=(
-                        "Keep NSFW content to a minimum. "
-                        "Only mild NSFW is allowed, and any NSFW media must be spoilered."
-                    ),
-                ),
-                discord.Embed(
-                    color=discord.Color.fuchsia(),
-                    title="Rule 4",
-                    description=(
-                        f"Avoid excessive spam outside <#{self.config['channels']['bot_commands']}> "
-                        f"or the appropriate channels."
-                    ),
-                ),
-                discord.Embed(
-                    color=discord.Color.fuchsia(),
-                    title="Rule 5",
-                    description="If you post potentially sensitive/triggering content, please spoiler it.",
-                ),
-                discord.Embed(
-                    color=discord.Color.fuchsia(),
-                    title="Rule 6",
-                    description="Please don't invite anyone without my permission.",
-                ),
+                ephemeral=True,
+            )
+            return
 
-            ]
+        await interaction.channel.send(
+            embeds=[discord.Embed(
+                color=discord.Color.fuchsia(),
+                title=f"Rule {i + 1}",
+                description=rule,
+            ) for i, rule in enumerate(rules)]
         )
 
         await interaction.response.send_message(
